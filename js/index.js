60ad532cc858bcaedcb850730b80a690f9ea65d6
Matheus Antônio <103318183+Matheus0902@users.noreply.github.com>
2024-05-09 18:09:42 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ import { Router } from "./router.js";
 
 const router = new Router()
 
-router.add("/", "./pages/home.html")
-router.add("/explorer", "./pages/explorer.html")
-router.add("/universe", "./pages/universe.html")
-router.add(404, "./pages/404.html")
+router.add("/", "../pages/home.html")
+router.add("/explorer", "../pages/explorer.html")
+router.add("/universe", "../pages/universe.html")
+router.add(404, "../pages/404.html")
 
 router.handle()
 
